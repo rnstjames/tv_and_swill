0fbe6bdd162f948b3447bb3d24fafe78fcdd8447
rnstjames <rnicoll@brandeis.edu>
2016-05-29 09:55:59 -0400

    Formats list and dictionary for readability

--- read_wine_matrix.py
+++ read_wine_matrix.py
@@ -4,24 +4,37 @@ value matrix into a dictionary and the genres into
 a list and then write these to a file
 """
 
-
-filename = input("Enter a filename to be read: ") #/Users/Ryan/Desktop/wine_matrix.csv
+filename = input("Enter a filename to be read: ")
 
 with open(filename) as f:
     contents = f.readlines()
-    genres = contents[0].strip("\n").split(",")[1:] # removes line breaks and initial formatting column
+    genres = contents[0].strip("\n").split(",")[1:]
     wine_dict = {}
     for line in contents[1:]:
         line = line.strip("\n").split(",")
         wine_dict[line[0]] = [float(x) for x in line[1:]]
         
-write = input("Enter a filename to write to: ")
+write_file = input("Enter a file to write to: ")
 
-with open(write, "w") as f2:
+with open("wine_data.py", "w") as f2:
     f2.write("genre = ")
+    len_list = len(genres)
+    f2.write("[\n")
+    while len_list > 0:
+        row = 5
+        while row > 0 and len_list > 0:
+            f2.write("'" + genres[len(genres) - len_list]+ "', ")
+            row -= 1
+            len_list -= 1
+        f2.write("\n")
+    f2.write("]")
     f2.write("\n")
     f2.write("wine_dict = ")
-    f2.write(str(wine_dict))
+    f2.write("{\n")
+    for key, value in wine_dict.items():
+        f2.write("'" + key + "': ")
+        f2.write(str(value) + ",\n")
+    f2.write("}\n")
     f2.write("\n")
 
 
